02eb1cda17a6362778c358a08cbf9689df4f6149
Norayr <gnorayr2002@gmail.com>
2022-02-19 19:21:59 +0300

    working already

--- Lab_1/random.cpp
+++ Lab_1/random.cpp
@@ -3,18 +3,19 @@
 #include <random>
 
 
-const int n = 10;
-const int m = 10;
+const int n = 5;
+const int m = 5;
 std::mt19937 rng(std::chrono::system_clock::now().time_since_epoch().count());
-int num = 15;
+int num = 1;
 
 
 void matrix(int a[n][m]) {
     int number = 0;
     while (number != num) {
-        std::uniform_int_distribution<int> dist(0, n);
-        int x = dist(rng);
-        int y = dist(rng);
+        std::uniform_int_distribution<int> distn(0, n - 1);
+        std::uniform_int_distribution<int> distm(0, m - 1);
+        int x = distn(rng);
+        int y = distm(rng);
         a[x][y] = 1;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
@@ -33,10 +34,19 @@ void move(int a[n][m]) {
             if (a[i][j] == 0) {
                 continue;
             }
-            if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+            if (j == 0 || j == m - 1) {
                 continue;
             }
-            if (a[i + 1][j] == 1 || a[i - 1][j] == 1 || a[i][j + 1] == 1 || a[i][j - 1] == 1)
+            if (n != 1) {
+                if (i == 0 || i == n - 1) {
+                    continue;
+                }
+            }
+            if (n != 1) {
+                if (a[i + 1][j] == 1 || a[i - 1][j] == 1)
+                    continue;
+            }
+            if (a[i][j + 1] == 1 || a[i][j - 1] == 1)
                 continue;
             a[i][j] = 2;
         }
@@ -45,41 +55,77 @@ void move(int a[n][m]) {
         for (int j = 0; j < m; ++j) {
             if (a[i][j] == 2) {
                 std::uniform_int_distribution<int> dist(0, 3);
-                int random = dist(rng);
-                if (random == 0 && a[i + 1][j] != 1) {
-                    a[i][j] = 0;
-                    a[i + 1][j] = 1;
-                }
-                if (random == 1 && a[i - 1][j] != 1) {
-                    a[i][j] = 0;
-                    a[i - 1][j] = 1;
-                }
-                if (random == 2 && a[i][j + 1] != 1) {
-                    a[i][j] = 0;
-                    a[i][j + 1] = 1;
-                }
-                if (random == 3 && a[i][j - 1] != 1) {
-                    a[i][j] = 0;
-                    a[i][j - 1] = 1;
+                std::uniform_int_distribution<int> dist_1(0, 1);
+                if (n != 1) {
+                    int random = dist(rng);
+                    if (random == 0 && a[i + 1][j] != 1) {
+                        a[i][j] = 0;
+                        a[i + 1][j] = 1;
+                    }
+                    if (random == 1 && a[i - 1][j] != 1) {
+                        a[i][j] = 0;
+                        a[i - 1][j] = 1;
+                    }
+                    if (random == 2 && a[i][j + 1] != 1) {
+                        a[i][j] = 0;
+                        a[i][j + 1] = 1;
+                    }
+                    if (random == 3 && a[i][j - 1] != 1) {
+                        a[i][j] = 0;
+                        a[i][j - 1] = 1;
+                    }
+                } else {
+                    int random = dist_1(rng);
+                    if (random == 0 && a[i][j + 1] != 1) {
+                        a[i][j] = 0;
+                        a[i][j + 1] = 1;
+                    }
+                    if (random == 1 && a[i][j - 1] != 1) {
+                        a[i][j] = 0;
+                        a[i][j - 1] = 1;
+                    }
+
                 }
             }
         }
     }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (a[i][j] == 2)
+                a[i][j] = 1;
+        }
+    }
+
 }
 
 
 int main() {
-    int step;
+    int step = 0;
+    int coincidence = 0;
     int a[n][m] = {};
+    int b[n][m] = {};
     matrix(a);
-    for (int k = 0; k < n; ++k) {
+    while (true) {
+        ++step;
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j)
+            for (int j = 0; j < m; ++j) {
+                b[i][j] = a[i][j];
                 std::cout << a[i][j] << " ";
+            }
             std::cout << std::endl;
         }
         move(a);
         std::cout << std::endl;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (a[i][j] == b[i][j])
+                    ++coincidence;
+            }
+        }
+        if (coincidence == m * n)
+            break;
+        else
+            coincidence = 0;
     }
+    std::cout << "steps =" << " "<< step << " ";
 }
-
